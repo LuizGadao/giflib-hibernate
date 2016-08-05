ca49434deae4acc8a559f6ec35d91dce7b325444
Luiz Carlos <luizgadao@gmail.com>
2016-08-04 21:46:44 -0300

    externalizing validation messages

--- src/main/java/com/teamtreehouse/giflib/config/AppConfig.java
+++ src/main/java/com/teamtreehouse/giflib/config/AppConfig.java
@@ -2,17 +2,42 @@ package com.teamtreehouse.giflib.config;
 
 import org.hashids.Hashids;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.core.env.Environment;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @Configuration
 @PropertySource("app.properties")
-public class AppConfig {
+public class AppConfig extends WebMvcConfigurerAdapter {
     @Autowired
     private Environment env;
+
+    @Bean
+    public MessageSource messageSource(){
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validator(){
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+
+    @Override
+    public Validator getValidator() {
+        return validator();
+    }
 
     @Bean
     public Hashids hashids() {
